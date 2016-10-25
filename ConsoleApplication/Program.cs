447c0c2ecb594791260e47a5e1edde99bcaf15cf
Glendon Cheney <glendon.cheney@gmail.com>
2016-10-25 13:02:41 -0500

    Add string function

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,30 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Glen";                  
+            var greeting = "Hello";          
+            Greeting(greeting, name, "loud");   
+            Greeting(greeting, name, "quiet"); 
+            string volume = String.Empty; // empty string
+            Greeting(greeting, name, volume);           
+            Greeting(greeting, name, "medium");       
+        }
+
+        public static void Greeting(string greeting, string name, string volume) 
+        {
+            if (String.IsNullOrEmpty(volume)) {
+                Console.WriteLine($"{greeting} {name}!");
+            } else {
+                if (volume.Equals("loud")) {
+                    var loud = $"{greeting} {name}!".ToUpper();
+                    Console.WriteLine(loud);
+                } else if (volume.Equals("quiet")) {
+                    var quiet = $"{greeting} {name}!".ToLower();
+                    Console.WriteLine(quiet);
+                } else {
+                    Console.WriteLine("Invalid input supplied for volume");
+                }
+            }
         }
     }
 }
